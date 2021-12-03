049439cc5e6b4ae88d7caeead57bb8ba0dd77a17
Jonathan Ströbele <mail@jonathanstroebele.de>
2021-12-03 14:00:44 +0100

    chore: template that copies inputs to output folder

---
+++ .template/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A();
+            B();
+        }
+
+        static void A()
+        {
+            foreach (string line in System.IO.File.ReadLines(@"input.txt"))
+            {
+
+            }
+
+        }
+        
+        static void B()
+        {
+
+        }
+    }
+}
